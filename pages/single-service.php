5658c057da5b1434df0264b6795262330f88a45f
yofanalfiatur <60646990+yofanalfiatur@users.noreply.github.com>
2024-09-27 22:45:48 +0700

    update : first update

--- pages/single-service.php
+++ pages/single-service.php
@@ -2,9 +2,78 @@
 $title = "Detail Service | Raksa Online";
 include "./components/header.php"; ?>
 
-<section>
+<section class="sg-serv-info">
     <div class="container">
-        <h1 class="">Detail Service</h1>
+        <div class="sg-serv-info__wrapper row">
+
+        </div>
+    </div>
+</section>
+
+<section class="sg-serv-loc">
+    <div class="container">
+        <div class="sg-serv-loc__wrapper row">
+            <h2 class="sg-serv-loc__title">Tersedia Di Lokasi Berikut</h2>
+        </div>
+    </div>
+</section>
+
+<section class="sg-serv-ct">
+    <div class="container">
+        <div class="sg-serv-loc__ct row gap-lg-2">
+            <div class="sg-serv-loc__ct__content col-12 col-lg-5">
+                <h2 class="sg-serv-loc__ct__content__title">Butuh Layanan Dek?</h2>
+                <a href="#" class="sg-serv-loc__ct__content__btn"></a>
+                <div class="sg-serv-loc__ct__content__contact">
+                    <a href="tel:085885558555" class="sg-serv-loc__ct__content__contact__wa" target="_blank">085885558555</a>
+                    <span>atau</span>
+                    <a href="tel:0217226865" class="sg-serv-loc__ct__content__contact__phone" target="_blank">021-7226865</a>
+                </div>
+            </div>
+            <div class="col-12 col-lg-5">
+                <img src="../dist/img/temp/service-card-4.jpg" alt="contact service">
+            </div>
+        </div>
+    </div>
+    <div class="sg-serv-ct__decor">
+        <img src="" alt="">
+    </div>
+</section>
+
+<section class="sg-serv-other">
+    <div class="container">
+        <div class="row">
+            <div class="sg-serv-other__intro col-12">
+                <h2 class="sg-serv-other__intro__title">Pelayanan Lainnya</h2>
+                <a href="#" class="sg-serv-other__intro__btn">Lihat Selengkapnya</a>
+            </div>
+            <div class="sg-serv-other__wrapper col-12 row">
+                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
+                    <a href="#" class="sg-serv-other__wrapper__card__link">
+                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
+                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
+                    </a>
+                </div>
+                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
+                    <a href="#" class="sg-serv-other__wrapper__card__link">
+                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
+                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
+                    </a>
+                </div>
+                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
+                    <a href="#" class="sg-serv-other__wrapper__card__link">
+                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
+                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
+                    </a>
+                </div>
+                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
+                    <a href="#" class="sg-serv-other__wrapper__card__link">
+                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
+                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
