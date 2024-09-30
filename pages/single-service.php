254949eeeeacd86c53937cabe6680073e84c3625
yofanalfiatur <60646990+yofanalfiatur@users.noreply.github.com>
2024-09-30 16:09:17 +0700

    update : service detail

--- pages/single-service.php
+++ pages/single-service.php
@@ -36,73 +36,117 @@ include "./components/global/banner-global.php"; ?>
                 <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-info__picture__img">
             </div>
         </div>
+        <div class="sg-serv-info__wrapper row">
+            <div class="sg-serv-info__content col-12 col-lg-7">
+                <h3 class="sg-serv-info__content__title">Sekilas Mengenai Raksa Gold Club</h3>
+                <div class="sg-serv-info__content__desc">
+                    <p>Diluncurkan sejak tahun 2004, Raksa Gold Club menjadi salah satu layanan eksklusif unggulan dari PT Asuransi Raksa. Layanan ini terbatas pada Nasabah dengan nilai pertanggungan mobil minimal $80.000. Tentu eksklusifitas dari Layanan kami kedepankan khusus untuk Nasabah di Raksa Gold Club. Tidak semua Nasabah dari Asuransi Raksa dapat mengakses layanan ini. Jadikan diri Anda sebagai Nasabah Raksa Gold Club untuk bisa merasakan Layanan Eksklusif dari PT Asuransi Raksa.</p>
+                </div>
+            </div>
+            <div class="sg-serv-info__picture col-12 col-lg-5">
+                <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-info__picture__img">
+            </div>
+        </div>
     </div>
 </section>
 
 <section class="sg-serv-loc">
     <div class="container">
-        <div class="sg-serv-loc__wrapper row">
-            <h2 class="sg-serv-loc__title">Tersedia Di Lokasi Berikut</h2>
+        <div class="sg-serv-loc__intro">
+            <h2 class="sg-serv-loc__intro__title">Tersedia Di Lokasi Berikut</h2>
+            <p class="sg-serv-loc__intro__desc">Mau klaim tapi jarak kantor Asuransi Raksa jauh? Jangan khawatir, Raksa Mobile Club akan memberikan kemudahan bagi nasabah dalam pengajuan klaim asuransi. Raksa Mobile Club hadir dalam bentuk mini bus yang didesain sedemikian rupa tampak seperti kantor berjalan dan berada di berbagai lokasi strategis di bawah ini :</p>
+
+            <div class="sg-serv-loc__map">
+                <div class="sg-serv-loc__map__card">
+                    <h4 class="sg-serv-loc__map__card__title">Cibubur</h4>
+                    <div class="sg-serv-loc__map__card__wrap">
+                        <p class="sg-serv-loc__map__card__desc">OMG Octavs Meat & Grill (Steak Cibubur) Jl. Alternatif Cibubur No.km 6 Cibubur Kec. Gn. Putri Bogor Jawa Barat 16967</p>
+                        <a href="https://maps.google.com/" target="_blank" class="sg-serv-loc__map__card__link">Lihat Di Google Maps</a>
+                    </div>
+                </div>
+                <div class="sg-serv-loc__map__card">
+                    <h4 class="sg-serv-loc__map__card__title">Cibubur</h4>
+                    <div class="sg-serv-loc__map__card__wrap">
+                        <p class="sg-serv-loc__map__card__desc">OMG Octavs Meat & Grill (Steak Cibubur) Jl. Alternatif Cibubur No.km 6 Cibubur Kec. Gn. Putri Bogor Jawa Barat 16967</p>
+                        <a href="https://maps.google.com/" target="_blank" class="sg-serv-loc__map__card__link">Lihat Di Google Maps</a>
+                    </div>
+                </div>
+                <div class="sg-serv-loc__map__card">
+                    <h4 class="sg-serv-loc__map__card__title">Cibubur</h4>
+                    <div class="sg-serv-loc__map__card__wrap">
+                        <p class="sg-serv-loc__map__card__desc">OMG Octavs Meat & Grill (Steak Cibubur) Jl. Alternatif Cibubur No.km 6 Cibubur Kec. Gn. Putri Bogor Jawa Barat 16967</p>
+                        <a href="https://maps.google.com/" target="_blank" class="sg-serv-loc__map__card__link">Lihat Di Google Maps</a>
+                    </div>
+                </div>
+                <div class="sg-serv-loc__map__card">
+                    <h4 class="sg-serv-loc__map__card__title">Cibubur</h4>
+                    <div class="sg-serv-loc__map__card__wrap">
+                        <p class="sg-serv-loc__map__card__desc">OMG Octavs Meat & Grill (Steak Cibubur) Jl. Alternatif Cibubur No.km 6 Cibubur Kec. Gn. Putri Bogor Jawa Barat 16967</p>
+                        <a href="https://maps.google.com/" target="_blank" class="sg-serv-loc__map__card__link">Lihat Di Google Maps</a>
+                    </div>
+                </div>
+                <div class="sg-serv-loc__map__card">
+                    <h4 class="sg-serv-loc__map__card__title">Cibubur</h4>
+                    <div class="sg-serv-loc__map__card__wrap">
+                        <p class="sg-serv-loc__map__card__desc">OMG Octavs Meat & Grill (Steak Cibubur) Jl. Alternatif Cibubur No.km 6 Cibubur Kec. Gn. Putri Bogor Jawa Barat 16967</p>
+                        <a href="https://maps.google.com/" target="_blank" class="sg-serv-loc__map__card__link">Lihat Di Google Maps</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </section>
 
 <section class="sg-serv-ct">
     <div class="container">
-        <div class="sg-serv-loc__ct row gap-lg-2">
-            <div class="sg-serv-loc__ct__content col-12 col-lg-5">
-                <h2 class="sg-serv-loc__ct__content__title">Butuh Layanan Dek?</h2>
-                <a href="#" class="sg-serv-loc__ct__content__btn"></a>
-                <div class="sg-serv-loc__ct__content__contact">
-                    <a href="tel:085885558555" class="sg-serv-loc__ct__content__contact__wa" target="_blank">085885558555</a>
+        <div class="sg-serv-ct__wrapper row">
+            <div class="sg-serv-ct__content col-12 col-lg-5">
+                <h2 class="sg-serv-ct__content__title">Butuh Layanan Dek?</h2>
+                <a href="#" class="sg-serv-ct__content__btn">
+                    <p class="sg-serv-ct__content__btn__text">Hubungi Kami</p>
+                </a>
+                <div class="sg-serv-ct__content__contact">
+                    <a href="tel:085885558555" class="sg-serv-ct__content__contact__phone" target="_blank">085885558555</a>
                     <span>atau</span>
-                    <a href="tel:0217226865" class="sg-serv-loc__ct__content__contact__phone" target="_blank">021-7226865</a>
+                    <a href="tel:0217226865" class="sg-serv-ct__content__contact__phone" target="_blank">021-7226865</a>
                 </div>
             </div>
-            <div class="col-12 col-lg-5">
-                <img src="../dist/img/temp/service-card-4.jpg" alt="contact service">
+            <div class="col-12 col-lg-5 sg-serv-ct__picture">
+                <img src="../dist/img/temp/service-card-4.jpg" alt="contact service" class="sg-serv-ct__picture__img">
             </div>
         </div>
     </div>
-    <div class="sg-serv-ct__decor">
-        <img src="" alt="">
-    </div>
+    <img src="../dist/img/temp/decor-card-serv.png" alt="" class="sg-serv-ct__decor">
 </section>
 
 <section class="sg-serv-other">
     <div class="container">
-        <div class="row">
-            <div class="sg-serv-other__intro col-12">
-                <h2 class="sg-serv-other__intro__title">Pelayanan Lainnya</h2>
-                <a href="#" class="sg-serv-other__intro__btn">Lihat Selengkapnya</a>
+        <div class="sg-serv-other__intro">
+            <h2 class="sg-serv-other__intro__title">Pelayanan Lainnya</h2>
+            <a href="#" class="sg-serv-other__intro__btn">Lihat Selengkapnya</a>
+        </div>
+        <div class="sg-serv-other__wrapper row">
+            <div class="sg-serv-other__card col-12 col-lg-4">
+                <a href="#" class="sg-serv-other__card__link">
+                    <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__card__img">
+                    <p class="sg-serv-other__card__title">Raksa Silver Club</p>
+                </a>
             </div>
-            <div class="sg-serv-other__wrapper col-12 row">
-                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
-                    <a href="#" class="sg-serv-other__wrapper__card__link">
-                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
-                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
-                    </a>
-                </div>
-                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
-                    <a href="#" class="sg-serv-other__wrapper__card__link">
-                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
-                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
-                    </a>
-                </div>
-                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
-                    <a href="#" class="sg-serv-other__wrapper__card__link">
-                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
-                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
-                    </a>
-                </div>
-                <div class="sg-serv-other__wrapper__card col-12 col-lg-4">
-                    <a href="#" class="sg-serv-other__wrapper__card__link">
-                        <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__wrapper__card__img">
-                        <p class="sg-serv-other__wrapper__card__title">Raksa Silver Club</p>
-                    </a>
-                </div>
+            <div class="sg-serv-other__card col-12 col-lg-4">
+                <a href="#" class="sg-serv-other__card__link">
+                    <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__card__img">
+                    <p class="sg-serv-other__card__title">Raksa Silver Club</p>
+                </a>
+            </div>
+            <div class="sg-serv-other__card col-12 col-lg-4">
+                <a href="#" class="sg-serv-other__card__link">
+                    <img src="../dist/img/temp/service-card-1.jpg" alt="" class="sg-serv-other__card__img">
+                    <p class="sg-serv-other__card__title">Raksa Silver Club</p>
+                </a>
             </div>
         </div>
+
     </div>
 </section>
 
